656fffe680d8c7ebd0f7201704e9b7af5aa8264d
GhexterCortes <ghextershumies@gmail.com>
2022-08-16 14:28:46 +0800

    Base and fetch members

---
+++ src/BaseModule.ts
@@ -0,0 +1,6 @@
+import { AnyCommandBuilder, RecipleScript } from 'reciple';
+
+export default class BaseModule implements Partial<RecipleScript> {
+    public versions: string|string[] = ['^5.1.1'];
+    public commands: AnyCommandBuilder[] = [];
+}
