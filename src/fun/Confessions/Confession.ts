493c2c57fb229c4cd7cb6137ef00bce82c5587c0
Ghex <ghextershumies@gmail.com>
2022-09-23 14:27:00 +0800

    confessions?

---
+++ src/fun/Confessions/Confession.ts
@@ -0,0 +1,63 @@
+import { Confessions, PrismaClient } from '@prisma/client';
+import { GuildTextBasedChannel, If, TextBasedChannel, User } from 'discord.js';
+import { RecipleClient } from 'reciple';
+import util from '../../tools/util';
+import { ConfessionsModule } from '../confessions';
+
+export interface RawConfession extends Confessions {}
+
+export class Confession<Fetched extends boolean = boolean> implements RawConfession {
+    private _author: User|null = null;
+    private _channel: GuildTextBasedChannel|null = null;
+    private _deleted: boolean = false;
+    private _authorId: string;
+    private _channelId: string;
+    private _content: string;
+    private _createdAt: Date;
+    private _id: string;
+
+    public confessionManager: ConfessionsModule;
+    public client: RecipleClient<true>;
+    public prisma: PrismaClient;
+
+    get author(): If<Fetched, User> { return this._author as If<Fetched, User>; }
+    get channel(): If<Fetched, GuildTextBasedChannel> { return this._channel as If<Fetched, GuildTextBasedChannel>; }
+    get deleted() { return this._deleted; }
+    get authorId() { return this._authorId; }
+    get channelId() { return this._channelId; }
+    get content() { return this._content; }
+    get createdAt() { return this._createdAt; }
+    get id() { return this._id; }
+
+    constructor(confessionManager: ConfessionsModule, rawConfession: RawConfession) {
+        this.confessionManager = confessionManager;
+        this.client = util.client;
+        this.prisma = util.prisma;
+
+        this._authorId = rawConfession.authorId;
+        this._channelId = rawConfession.channelId;
+        this._content = rawConfession.content;
+        this._createdAt = rawConfession.createdAt;
+        this._id = rawConfession.id;
+    }
+
+    public async fetch(): Promise<Confession<true>> {
+        if (!this.isFetched()) throw new Error('Cannot fetch confession');
+        return this;
+    }
+
+    public isFetched(): this is Confession<true> {
+        return this._author !== null && this._channel !== null;
+    }
+
+    public async delete(): Promise<void> {
+        await this.prisma.confessions.delete({
+            where: {
+                id: this.id
+            }
+        });
+
+        this._deleted = true;
+        this.confessionManager.cache.sweep(c => c.deleted);
+    }
+}
