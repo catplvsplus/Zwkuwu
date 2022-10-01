9a15d97de7c018db72d1e4d9269c3dbbf4479621
Ghex <ghextershumies@gmail.com>
2022-10-01 14:50:56 +0800

    ping command

---
+++ src/ping.ts
@@ -0,0 +1,32 @@
+import { EmbedBuilder } from 'discord.js';
+import ms from 'ms';
+import { MessageCommandBuilder, RecipleClient, SlashCommandBuilder } from 'reciple';
+import BaseModule from './BaseModule';
+import util from './tools/util';
+
+export class PingModule extends BaseModule {
+    public async onStart(client: RecipleClient<boolean>): Promise<boolean> {
+        this.commands = [
+            new SlashCommandBuilder()
+                .setName('ping')
+                .setDescription('Show bot\'s ping')
+                .setExecute(async data => {
+                    await data.interaction.reply({ embeds: [this.getPing(client)] });
+                }),
+            new MessageCommandBuilder()
+                .setName('ping')
+                .setDescription('Show bot\'s ping')
+                .setExecute(async data => {
+                    await data.message.reply({ embeds: [this.getPing(client)] })
+                })
+        ];
+
+        return true;
+    }
+
+    public getPing(client: RecipleClient<true>): EmbedBuilder {
+        return util.smallEmbed(`Pong ┃ ${ms(client.ws.ping, { long: true })}`);
+    }
+}
+
+export default new PingModule();
