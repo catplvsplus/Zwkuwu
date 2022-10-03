944728af1109d69668d4ccfa9b16c42f5a55f142
Ghex <ghextershumies@gmail.com>
2022-10-03 10:45:45 +0800

    untested but ok

---
+++ src/tools/zombieConnectionHandler.ts
@@ -0,0 +1,26 @@
+import { RecipleClient } from 'reciple';
+import BaseModule from '../BaseModule';
+
+export class ZombieConnectionHandlerModule extends BaseModule {
+    public lastPing: number = 0;
+    public checkInterval: number = 50000;
+    public timer?: NodeJS.Timer;
+
+    public async onStart(client: RecipleClient<boolean>): Promise<boolean> {
+        return true;
+    }
+
+    public async onLoad(client: RecipleClient<boolean>): Promise<void> {
+        this.timer = setInterval(() => this.checkLastPing(client), this.checkInterval);
+    }
+
+    public checkLastPing(client: RecipleClient<boolean>): number {
+        if (client.ws.ping === this.lastPing) {
+            client.logger.error(`Exiting process! Client ws ping is a zombie connection.`);
+            process.exit(1);
+        }
+
+        this.lastPing = client.ws.ping;
+        return this.lastPing;
+    }
+}
