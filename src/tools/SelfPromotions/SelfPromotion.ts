1533b63da8f9a190a14b3ff799cd681db2b64f37
Ghex <ghextershumies@gmail.com>
2022-10-06 19:33:13 +0800

    self promotions

--- src/tools/SelfPromotions/SelfPromotion.ts
+++ src/tools/SelfPromotions/SelfPromotion.ts
@@ -1,3 +1,149 @@
-export class SelfPromotion {
-    
+import { PrismaClient, SelfPromotions as PrismaSelfPromotions } from '@prisma/client';
+import { If, Message, MessageCreateOptions, TextBasedChannel, User } from 'discord.js';
+import { RecipleClient } from 'reciple';
+import { SelfPromotionsModule } from '../selfPromotions';
+import util from '../util';
+
+export interface RawSelfPromotion extends PrismaSelfPromotions {}
+
+export class SelfPromotion<Fetched extends boolean = boolean> implements RawSelfPromotion {
+    private _author: User|null = null;
+    private _approvedBy: User|null = null;
+    private _approvedMessage: Message|null = null;
+    private _approvedChannel: TextBasedChannel|null = null;
+    private _deleted: boolean = false;
+    private _id: string;
+    private _authorId: string;
+    private _content: string;
+    private _createdAt: Date;
+    private _approvedAt: Date|null;
+    private _approvedById: string|null;
+    private _approvedMessageId: string|null;
+    private _approvedChannelId: string|null;
+
+    readonly selfPromotionsModule: SelfPromotionsModule;
+    readonly prisma: PrismaClient;
+    readonly client: RecipleClient<true>;
+
+    get author(): If<Fetched, User> { return this._author as If<Fetched, User>; };
+    get approvedBy() { return this._approvedBy; };
+    get approvedMessage() { return this._approvedMessage; };
+    get approvedChannel() { return this._approvedChannel; };
+    get deleted() { return this._deleted; };
+    get id() { return this._id; }
+    get authorId() { return this._authorId; }
+    get content() { return this._content; }
+    get createdAt() { return this._createdAt; }
+    get approvedAt() { return this._approvedAt; }
+    get approvedById() { return this._approvedById; }
+    get approvedMessageId() { return this._approvedMessageId; }
+    get approvedChannelId() { return this._approvedChannelId; }
+
+    constructor(selfPromotionsModule: SelfPromotionsModule, rawSelfPromotion: RawSelfPromotion) {
+        this.selfPromotionsModule = selfPromotionsModule;
+        this.prisma = util.prisma;
+        this.client = util.client;
+
+        this._id = rawSelfPromotion.id;
+        this._authorId = rawSelfPromotion.authorId;
+        this._content = rawSelfPromotion.content;
+        this._createdAt = rawSelfPromotion.createdAt;
+        this._approvedAt = rawSelfPromotion.approvedAt;
+        this._approvedById = rawSelfPromotion.approvedById;
+        this._approvedMessageId = rawSelfPromotion.approvedMessageId;
+        this._approvedChannelId = rawSelfPromotion.approvedChannelId;
+    }
+
+    public async fetch(): Promise<SelfPromotion<true>> {
+        const data = await this.prisma.selfPromotions.findFirst({
+            where: { id: this.id }
+        });
+
+        if (!data) {
+            await this.delete()
+            throw new Error(`No self promotion data from database`);
+        }
+
+        this._id = data.id;
+        this._authorId = data.authorId;
+        this._content = data.content;
+        this._createdAt = data.createdAt;
+        this._approvedAt = data.approvedAt;
+        this._approvedById = data.approvedById;
+        this._approvedMessageId = data.approvedMessageId;
+        this._approvedChannelId = data.approvedChannelId;
+
+        const author = await this.client.users.fetch(this.authorId).catch(() => undefined);
+        const approvedBy = this.approvedById ? await this.client.users.fetch(this.approvedById).catch(() => undefined) : null;
+        const approvedChannel = this.approvedChannelId ? await this.client.channels.fetch(this.approvedChannelId).catch(() => undefined) : null;
+
+        if (author === undefined) throw new Error('Cannot fetch self promotion author');
+        if (approvedBy === undefined) throw new Error('Cannot fetch self promotion approvedBy');
+        if (approvedChannel === undefined || approvedChannel && !approvedChannel.isTextBased()) throw new Error('Cannot fetch self promotion approvedChannel');
+
+        this._author = author;
+        this._approvedBy = approvedBy;
+        this._approvedChannel = approvedChannel;
+
+        const approvedMessage = this.approvedMessageId ? await this.approvedChannel?.messages.fetch(this.approvedMessageId).catch(() => undefined) : null;
+
+        if (approvedMessage === undefined) throw new Error('Cannot fetch self promotion message');
+
+        this._approvedMessage = approvedMessage;
+
+        if (!this.isFetched()) throw new Error('Cannot fetch self promotion data');
+        return this;
+    }
+
+    public isFetched(): this is SelfPromotion<true> {
+        return this.author !== null && (!this.approvedAt || this.approvedBy !== null && this._approvedMessage !== null && this._approvedChannel !== null);
+    }
+
+    public async approve(sendTo: TextBasedChannel, approvedBy: User, messageOptions?: Partial<MessageCreateOptions>): Promise<void> {
+        if (!this.isFetched()) throw new Error('Self promotion is not fetched');
+
+        const message = await sendTo.send({
+            allowedMentions: {
+                parse: ['users']
+            },
+            ...messageOptions,
+            content: `${this}`
+        });
+
+        this._approvedAt = message.createdAt;
+        this._approvedBy = approvedBy;
+        this._approvedById = approvedBy.id;
+        this._approvedChannel = sendTo;
+        this._approvedChannelId = sendTo.id;
+        this._approvedMessage = message;
+        this._approvedMessageId = message.id;
+
+        await this.prisma.selfPromotions.update({
+            where: { id: this.id },
+            data: {
+                approvedAt: this.approvedAt,
+                approvedById: this.approvedById,
+                approvedChannelId: this.approvedChannelId,
+                approvedMessageId: this.approvedMessageId
+            }
+        }).catch(async err => {
+            await message.delete().catch(() => {});
+            throw err;
+        });
+    }
+
+    public async delete(): Promise<void> {
+        await this.prisma.selfPromotions.delete({
+            where: { id: this.id }
+        });
+
+        this._deleted = true;
+
+        this.selfPromotionsModule.cache.sweep(s => s.deleted);
+        await this.approvedMessage?.delete().catch(() => {});
+    }
+
+    public toString(): string {
+        return `**Author:** ${this.author}\n\n${this.content}`;
+    }
 }
--- src/tools/selfPromotions.ts
+++ src/tools/selfPromotions.ts
@@ -1,12 +1,293 @@
-import { Collection } from 'discord.js';
-import { RecipleClient } from 'reciple';
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, Collection, Message, PermissionResolvable, TextBasedChannel } from 'discord.js';
+import { cwd, RecipleClient, SlashCommandBuilder } from 'reciple';
 import BaseModule from '../BaseModule';
-import { SelfPromotion } from './SelfPromotions/SelfPromotion';
+import { RawSelfPromotion, SelfPromotion } from './SelfPromotions/SelfPromotion';
+import util from './util';
+import yml from 'yaml';
+import createConfig from '../_createConfig';
+import path from 'path';
+import { setTimeout } from 'timers/promises';
+import { InteractionEventType } from './InteractionEvents';
+
+export interface SelfPromotionsConfig {
+    promotionsChannel: string;
+    pendingApprovalChannel: string;
+    ignoreBots: boolean;
+    requiredApproverPermisions: PermissionResolvable[];
+}
 
 export class SelfPromotionsModule extends BaseModule {
     public cache: Collection<string, SelfPromotion> = new Collection();
+    public config: SelfPromotionsConfig = SelfPromotionsModule.getConfig();
+    public pendingApprovalChannel?: TextBasedChannel;
+    public promotionsChannel?: TextBasedChannel;
 
     public async onStart(client: RecipleClient<boolean>): Promise<boolean> {
+        this.commands = [
+            new SlashCommandBuilder()
+                .setName('self-promotion')
+                .setDescription('Manage self promotions')
+                .setRequiredMemberPermissions(this.config.requiredApproverPermisions)
+                .addSubcommand(approve => approve
+                    .setName('approve')
+                    .setDescription('Approve pending self promotion')
+                    .addStringOption(selfpromotion => selfpromotion
+                        .setName('selfpromotion')
+                        .setDescription('Self promotion to approve')
+                        .setAutocomplete(true)
+                        .setRequired(true)
+                    )
+                )
+                .addSubcommand(deny => deny
+                    .setName('deny')
+                    .setDescription('Deny pending self promotion')
+                    .addStringOption(selfpromotion => selfpromotion
+                        .setName('selfpromotion')
+                        .setDescription('Self promotion to deny')
+                        .setAutocomplete(true)
+                        .setRequired(true)
+                    )
+                )
+                .addSubcommand(preview => preview
+                    .setName('preview')
+                    .setDescription('Preview pending self promotion')
+                    .addStringOption(selfpromotion => selfpromotion
+                        .setName('selfpromotion')
+                        .setDescription('Self promotion to preview')
+                        .setAutocomplete(true)
+                        .setRequired(true)
+                    )
+                )
+                .setExecute(async data => {
+                    const interaction = data.interaction;
+                    const command = interaction.options.getSubcommand() as 'approve'|'deny'|'preview';
+                    const selfPromotionId = interaction.options.getString('selfpromotion', true);
+
+                    await interaction.deferReply({ ephemeral: true });
+
+                    const promotion = await this.resolveSelfPromotion(selfPromotionId);
+
+                    if (!promotion) {
+                        await interaction.editReply({ embeds: [util.errorEmbed('No self promotion data found')] });
+                        return;
+                    }
+
+                    switch (command) {
+                        case 'preview':
+                            interaction.editReply(promotion.toString());
+                            return;
+                        case 'approve':
+                            if (!this.promotionsChannel) {
+                                await interaction.editReply({ embeds: [util.errorEmbed('No promotions channel specified')] });
+                                return;
+                            }
+
+                            if (promotion.approvedAt) {
+                                await interaction.editReply({ embeds: [util.errorEmbed(`Already approved by **${promotion.approvedBy || 'Unknown User'}**`, true)] });
+                                return;
+                            }
+
+                            await promotion.approve(this.promotionsChannel, interaction.user);
+                            await interaction.editReply({ embeds: [util.smallEmbed('Self promotion approved')] });
+                            return;
+                        case 'deny':
+                            await promotion.delete();
+                            await interaction.editReply({ embeds: [util.smallEmbed('Self promotion denied')] });
+                            return;
+                    }
+                })
+        ];
+
+        this.interactionEventHandlers = [
+            {
+                type: InteractionEventType.Button,
+                customId: id => id.startsWith(`approve-selfpromotion`),
+                handle: async interaction => {
+                    if (!interaction.isButton()) return;
+
+                    await interaction.deferReply({ ephemeral: true });
+                    const promotionId = interaction.customId.split('-')[2];
+                    const promotion = await this.resolveSelfPromotion(promotionId);
+
+                    if (!promotion) {
+                        await interaction.editReply({ embeds: [util.errorEmbed('No self promotion data found')] });
+                        return;
+                    }
+
+                    if (!this.promotionsChannel) {
+                        await interaction.editReply({ embeds: [util.errorEmbed('No promotions channel specified')] });
+                        return;
+                    }
+
+                    if (this.config.requiredApproverPermisions && !interaction.memberPermissions?.has(this.config.requiredApproverPermisions)) {
+                        await interaction.editReply({ embeds: [util.errorEmbed('No permissions')] });
+                        return;
+                    }
+
+                    await interaction.message.delete();
+
+                    if (promotion.approvedAt) {
+                        await interaction.editReply({ embeds: [util.errorEmbed(`Already approved by **${promotion.approvedBy || 'Unknown User'}**`, true)] });
+                        return;
+                    }
+
+                    await promotion.approve(this.promotionsChannel, interaction.user);
+                    await interaction.editReply({ embeds: [util.smallEmbed(`Self promotion approved`)] });
+                }
+            },
+            {
+                type: InteractionEventType.Button,
+                customId: id => id.startsWith(`deny-selfpromotion`),
+                handle: async interaction => {
+                    if (!interaction.isButton()) return;
+
+                    await interaction.deferReply({ ephemeral: true });
+                    const promotionId = interaction.customId.split('-')[2];
+                    const promotion = await this.resolveSelfPromotion(promotionId);
+
+                    if (!promotion) {
+                        interaction.editReply({ embeds: [util.errorEmbed('No self promotion data found')] });
+                        return;
+                    }
+
+                    if (this.config.requiredApproverPermisions && !interaction.memberPermissions?.has(this.config.requiredApproverPermisions)) {
+                        await interaction.editReply({ embeds: [util.errorEmbed('No permissions')] });
+                        return;
+                    }
+
+                    await interaction.message.delete();
+                    await promotion.delete();
+                    await interaction.editReply({ embeds: [util.smallEmbed(`Self promotion denied`)] });
+                }
+            },
+            {
+                type: InteractionEventType.AutoComplete,
+                commandName: 'self-promotion',
+                handle: async interaction => {
+                    if (!interaction.isAutocomplete()) return;
+
+                    const query = interaction.options.getFocused();
+                    const data = await util.prisma.selfPromotions.findMany({
+                        where: query
+                            ? {
+                                approvedAt: null,
+                                id: {
+                                    contains: query
+                                },
+                                OR: {
+                                    content: {
+                                        contains: query
+                                    }
+                                }
+                            }
+                            : {
+                                approvedAt: null
+                            },
+                        orderBy: {
+                            createdAt: 'desc'
+                        },
+                        take: 15
+                    });
+
+                    await interaction.respond(data.map(s => ({
+                        name: s.id,
+                        value: s.id
+                    })));
+                }
+            }
+        ];
+
         return true;
     }
+
+    public async onLoad(client: RecipleClient<boolean>): Promise<void> {
+        this.pendingApprovalChannel = await client.channels.fetch(this.config.pendingApprovalChannel).then(c => c?.isTextBased() ? c : undefined).catch(() => undefined);
+        this.promotionsChannel = await client.channels.fetch(this.config.promotionsChannel).then(c => c?.isTextBased() ? c : undefined).catch(() => undefined);
+
+        client.on('messageCreate', async message => {
+            if (this.config.promotionsChannel !== message.channel.id) return;
+            if (this.config.ignoreBots && message.author.bot) return;
+            if (message.author.id === client.user?.id) return;
+
+            await message.delete();
+
+            if (!message.content) return;
+
+            await this.addPendingPromotion(message);
+            await message.channel.send({ embeds: [util.smallEmbed(`Your self promotion is pending for approval`)] }).then(async msg => {
+                await setTimeout(5000);
+                await msg.delete();
+            });
+        });
+    }
+
+    public async addPendingPromotion(message: Message): Promise<SelfPromotion<true>> {
+        const selfPromotion = await this.createSelfPromotion(message);
+        if (!this.pendingApprovalChannel) return selfPromotion;
+
+        await this.pendingApprovalChannel.send({
+            embeds: [
+                util.smallEmbed('Pending Self Promotion Approval')
+                    .setDescription(selfPromotion.toString())
+            ],
+            components: [
+                new ActionRowBuilder<ButtonBuilder>()
+                    .setComponents(
+                        new ButtonBuilder() 
+                            .setCustomId(`approve-selfpromotion-${selfPromotion.id}`)
+                            .setLabel(`Approve`)
+                            .setStyle(ButtonStyle.Primary),
+                        new ButtonBuilder()
+                            .setCustomId(`deny-selfpromotion-${selfPromotion.id}`)
+                            .setLabel(`Deny`)
+                            .setStyle(ButtonStyle.Secondary)
+                    )
+            ]
+        });
+
+        return selfPromotion;
+    }
+
+    public async resolveSelfPromotion(id: string): Promise<SelfPromotion<true>|undefined> {
+        return this.cache.get(id) ?? this.fetchSelfPromotion(id);
+    }
+
+    public async fetchSelfPromotion(filter: string|Partial<RawSelfPromotion>, cache: boolean = true): Promise<SelfPromotion<true>|undefined> {
+        const data = await util.prisma.selfPromotions.findFirst({
+            where: typeof filter === 'string'
+                ? { id: filter }
+                : filter
+        });
+
+        if (!data) return undefined;
+
+        const selfPromotion = await (new SelfPromotion(this, data)).fetch();
+        if (cache) this.cache.set(selfPromotion.id, selfPromotion);
+
+        return selfPromotion;
+    }
+
+    public async createSelfPromotion(message: Message): Promise<SelfPromotion<true>> {
+        const data = await util.prisma.selfPromotions.create({
+            data: {
+                id: message.id,
+                content: message.content,
+                authorId: message.author.id,
+                createdAt: message.createdAt
+            }
+        });
+
+        return (await this.fetchSelfPromotion(data.id))!;
+    }
+
+    public static getConfig(): SelfPromotionsConfig {
+        return yml.parse(createConfig(path.join(cwd, 'config/selfpromotions/config.yml'), <SelfPromotionsConfig>({
+            pendingApprovalChannel: '000000000000000000',
+            promotionsChannel: '000000000000000000',
+            requiredApproverPermisions: ['ManageMessages'],
+            ignoreBots: true
+        })));
+    }
 }
+
+export default new SelfPromotionsModule();
